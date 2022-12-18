de9f5c17b711dfc16c54f818cf62bf8fb8eb8574
Olga B <>
2022-12-19 01:11:30 +0300

    edit hw_2.p

--- semi_1/task_2.py
+++ semi_1/task_2.py
@@ -1,9 +1,12 @@
 # 2. Напишите программу, которая на вход принимает 5 чисел и находит максимальное из них.
 
-max_num = 0
+my_list = []
+
+max_num = -100000000
 for i in range (5):
-    print('Введите число')
-    num=int(input())
+    num=int(input(f'Введите {i+1} число: '))
+    my_list.append(num) # добавляем с конец списка число
     if num > max_num:
         max_num=num
 print('Максимальное число: ', max_num)
+print(my_list)
